3776c9113027f08878944059839d14ee13f87c3e
Ari Yacobson <ariyacovson@gmail.com>
2023-09-27 17:44:05 +0300

    adding services folder and necessary packages for redis

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+from services import redis_service
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
